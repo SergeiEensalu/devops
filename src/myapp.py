240d66376f513ef0cbc0324e225dfbb54d490401
Sergei Eensalu <sergei.eensalu@gmail.com>
2022-02-15 17:27:06 +0200

    fast fix

--- src/myapp.py
+++ src/myapp.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-    1)heroku login
-    2)heroku autorizations:create
-    3) take token
-    4) github -> sdettings -> secretc -> action secrets -> new secret -> HEROKU_API_TOKEN -> add secret
-    5) heroku -> copy aplicaton name  -> github -> sdettings -> secretc -> action secrets -> new secret -> HEROKU_APP_NAME -> add secret
-    6) need be two secrets in github
-
-
-    7) git add
+    # 1)heroku login
+    # 2)heroku autorizations:create
+    # 3) take token
+    # 4) github -> sdettings -> secretc -> action secrets -> new secret -> HEROKU_API_TOKEN -> add secret
+    # 5) heroku -> copy aplicaton name  -> github -> sdettings -> secretc -> action secrets -> new secret -> HEROKU_APP_NAME -> add secret
+    # 6) need be two secrets in github
+    #
+    #
+    # 8) dobavit wto to tipo 'fetch all history for tags' v yml file
+    #
